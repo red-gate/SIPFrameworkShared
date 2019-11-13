2350ad12266b9861381faa37ab12ebd1e69c3e3c
Mark Raymond <mark.raymond@red-gate.com>
2019-11-13 11:54:08 +0000

    Allow passing in a connection string instead of a SqlConnection.

--- SIPFrameworkShared/IServerManagementObjectsAdapter.cs
+++ SIPFrameworkShared/IServerManagementObjectsAdapter.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Data.SqlClient;
 
 namespace RedGate.SIPFrameworkShared
 {
     public interface IServerManagementObjectsAdapter
     {
+        [Obsolete("Pass in a connection string, not an open SqlConnection")]
         string ScriptAsAlter(SqlConnection openSqlConnection, string databaseName, string schemaName, string scriptableObjectName);
+        string ScriptAsAlter(string connectionString, string databaseName, string schemaName, string scriptableObjectName);
     }
 }
