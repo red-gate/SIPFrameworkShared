c5b36c6cb0a3b43c22bfd0aa36bd85b899721834
Mark Raymond <mark.raymond@red-gate.com>
2019-11-13 13:06:37 +0000

    Use the SIPFw-style extending interfaces to maintain compatibility.

--- RELEASENOTES.md
+++ RELEASENOTES.md
@@ -1,3 +1,5 @@
-# 1.0
+# 1.1
+- ScriptAsAlter now takes a connection string instead of an open SqlConnection
 
+# 1.0
 - Introduce build script so that we can reproduce our builds
--- SIPFrameworkShared/IServerManagementObjectsAdapter.cs
+++ SIPFrameworkShared/IServerManagementObjectsAdapter.cs
@@ -7,6 +7,10 @@ namespace RedGate.SIPFrameworkShared
     {
         [Obsolete("Pass in a connection string, not an open SqlConnection")]
         string ScriptAsAlter(SqlConnection openSqlConnection, string databaseName, string schemaName, string scriptableObjectName);
+    }
+
+    public interface IServerManagementObjectsAdapter2 : IServerManagementObjectsAdapter
+    {
         string ScriptAsAlter(string connectionString, string databaseName, string schemaName, string scriptableObjectName);
     }
 }
